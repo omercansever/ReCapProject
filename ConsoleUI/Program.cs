81be2a7a846c0bac2d147de91d68575b46e3cea1
omercan <ocansever493@gmail.com>
2023-11-16 16:57:16 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemory());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
---
+++ ConsoleUı/Program.cs
@@ -0,0 +1,12 @@
+﻿using Business.Concrete;
+
+namespace ConsoleUı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              CarManager
+        }
+    }
+}
